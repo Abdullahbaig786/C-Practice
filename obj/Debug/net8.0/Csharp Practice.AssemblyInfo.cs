b0268f41840e0d278a7386345de4e896004d537f
Mirza Abdullah Baig <abdullah.baig2013@gmail.com>
2024-01-15 20:50:11 +0100

    !

--- obj/Debug/net8.0/Csharp Practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea541631ab4765ab95b9e6cd5e34963c3beac25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
